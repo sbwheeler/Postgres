1acbb32d519268fa652557a3f036166a8ed43dbb
Sam Wheeler <swheeler111@gmail.com>
2016-11-10 16:16:52 -0500

    added clickable tweets

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const pg = require('pg');
-const postgresUrl = 'postgres://localhost/Andres';
+const postgresUrl = 'postgres://localhost/twitterdb';
 var client = new pg.Client(postgresUrl);
 
 client.connect();
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ module.exports = io => {
 
   // a reusable function
   const respondWithAllTweets = (req, res, next) => {
-    client.query('SELECT name, content FROM tweets INNER JOIN users on tweets.userid=users.id', function (err, result) {
+    client.query('SELECT name, content, pictureurl, tweets.id FROM tweets INNER JOIN users on tweets.userid=users.id', function (err, result) {
     if (err) return next(err); // pass errors to Express
       var tweets = result.rows;
       res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
@@ -23,11 +23,11 @@ module.exports = io => {
   router.get('/tweets', respondWithAllTweets);
 
 
-  
+
   // single-user page
   router.get('/users/:username', (req, res, next) => {
     var name = req.params.username;
-    client.query('SELECT name, content FROM tweets INNER JOIN users on tweets.userid=users.id AND users.name=$1',[name], function (err, result) {
+    client.query('SELECT name, content, pictureurl FROM tweets INNER JOIN users on tweets.userid=users.id AND users.name=$1',[name], function (err, result) {
     if (err) return next(err); // pass errors to Express
       var tweets = result.rows;
       res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
@@ -36,10 +36,11 @@ module.exports = io => {
 
   // single-tweet page
   router.get('/tweets/:id', (req, res, next) => {
-    const tweetsWithThatId = tweetBank.find({ id: Number(req.params.id) });
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: tweetsWithThatId // an array of only one element ;-)
+    const tweet = req.params.id;
+    client.query('SELECT name, content, pictureurl FROM tweets INNER JOIN users on tweets.userid=users.id AND tweets.id=$1', [tweet], function (err,result) {
+      if (err) return next(err);
+      let tweets = result.rows;
+      res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
     });
   });
 
